b0d8560451be177c00ac47b087eebd8a9788886c
joekern <joekernphd@gmail.com>
2017-12-08 00:46:12 -0800

    part 1 & part 2 finished

--- 1-terminator/help.py
+++ 1-terminator/help.py
@@ -1,2 +1,4 @@
-var = 'A'*78+'BBBBCCCCDDDD' 
+var = ''
+for i in range(0, 20):
+  var = var + chr(ord('A')+i)*4
 print(var)
--- 2-rop/help.py
+++ 2-rop/help.py
@@ -1,4 +1,4 @@
 var = ''
-for i in range(0, 20):
+for i in range(0, 23):
   var = var + chr(ord('A')+i)*4
 print(var)
